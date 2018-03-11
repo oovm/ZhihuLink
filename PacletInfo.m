753a26c84ee7a78954c0fc71ac8d634f819f0cb0
GalAster <galaster@foxmail.com>
2018-03-12 00:22:51 +0800

    架构::打包工具PackPaclet close #1

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,5 +1,5 @@
 (* Created with the Wolfram Language : www.wolfram.com *)
-Paclet[Name -> "ZhihuLink", Version -> "0.0.2", WolframVersion -> "10+", 
+Paclet[Name -> "ZhihuLink", Version -> "0.0.6", WolframVersion -> "10+", 
  Internal -> False, SystemID -> All, 
  Extensions -> {{"Kernel", "Root" -> ".", "Context" -> {"ZhihuLink`"}, 
     Root -> ".", Context -> {"ZhihuLink`"}}}]
