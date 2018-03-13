d0319fe5761dcb5e3cf951ca43ba12e8f15b1163
GalAster <galaster@foxmail.com>
2018-03-13 17:23:07 +0800

    重构::打包工具 V2.5

--- Kernel/ZhihuLinkDeploy.sh
+++ Kernel/ZhihuLinkDeploy.sh
@@ -1,2 +1,22 @@
 #!/usr/bin/env bash
+#
 wolframscript -script ./ZhihuLinkBuild.wls
+#
+cd ../..
+rm -rf ZhihuLinkTemp
+cp -rf ZhihuLink ZhihuLinkTemp
+cd ZhihuLinkTemp
+#
+rm -rf .git
+rm -rf .idea
+rm -rf Resources
+rm -rf Packages/__Dev
+rm -rf Packages/__Raw
+rm -f .gitignore
+rm -f Kernel/ZhihuLink*
+#
+echo "Print@PackPaclet[DirectoryName@ExpandFileName[First[\$ScriptCommandLine]]]" > ZhihuLinkPack.wls
+echo "Packing:"
+wolframscript -script ./ZhihuLinkPack.wls
+echo "deploy finish, Ctrl+C to exit."
+sleep 600
